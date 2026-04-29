3089de6dda2bc91d1212d844c05ec9655515aac5
Gabriel Junqueira <161396206+GabrielMMVI@users.noreply.github.com>
2026-04-29 14:41:31 -0300

    Correção de busca
    Corrigi o script para buscar a player_hurtbox

--- ent/projectiles/magic_ball/scrMage_Magic_Ball.gd
+++ ent/projectiles/magic_ball/scrMage_Magic_Ball.gd
@@ -56,9 +56,8 @@ func _on_body_entered(body: Node2D) -> void:
 # ==============================================================================
 func _on_area_entered(area: Area2D) -> void:
 		# Detecta o aHurtbox do player pelo grupo
-	if area.is_in_group("player"):
+	if area.is_in_group("player_hurtbox"):
 		var player = area.get_parent()
 		if player.has_method("take_damage"):
 			player.take_damage(DAMAGE)
-			print("Tomou dano")
 		queue_free()
