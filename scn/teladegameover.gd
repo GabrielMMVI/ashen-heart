85b398ec02885070c888848244e1bf37bd35b032
miguelsilva101112 <161774578+miguelsilva101112@users.noreply.github.com>
2026-06-20 08:50:32 -0300

    Tela de gamer over funcionando
    Mexi na funcao die no script do player para que a tela apareça qunado o player morrer, criei scripts dos dois botoes da tela de game over e coloquei pressed() neles, para funcionar os dois botões

---
+++ scn/teladegameover.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+func _ready():
+	process_mode = Node.PROCESS_MODE_WHEN_PAUSED
+
+func _on_button_tentar_pressed():
+	get_tree().paused = false
+	get_tree().reload_current_scene()
+
+func _on_button_sair_over_pressed():
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scn/MainMenu.tscn")
